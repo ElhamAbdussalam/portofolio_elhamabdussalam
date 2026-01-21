a0125a8a511f71312a737b7292bbdd00ca86012f
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-01-21 20:25:04 +0700

    globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./global.css";
+import "./globals.css";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const inter = Inter({ subsets: ["latin"] });
